5ed70659124fd22e5755368654dfb8238b83496a
Ricardo Riogo <ricardoriogo@gmail.com>
2015-02-16 00:35:23 -0200

    Fix migration class name

--- src/views/migration.blade.php
+++ src/views/migration.blade.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRolesTable extends Migration {
+class PermisoTables extends Migration {
 
     /**
     * Run the migrations.
